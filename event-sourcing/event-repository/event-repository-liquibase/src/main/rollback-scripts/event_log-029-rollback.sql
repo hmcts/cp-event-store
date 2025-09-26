770678fa7a0212e653dea1dec67e2d09ca866bf1
zeldal-hmcts <zeldal.ozdemir1@hmcts.net>
2025-09-26 16:21:04 +0100

    Add event_log -> event_number default value changes.

--- event-sourcing/event-repository/event-repository-liquibase/src/main/rollback-scripts/event_log-029-rollback.sql
+++ event-sourcing/event-repository/event-repository-liquibase/src/main/rollback-scripts/event_log-029-rollback.sql
@@ -1,4 +1,4 @@
 -- Roll back liquibase script '029-change-event_log-event_number-definition.changelog.xml'
 
-ALTER TABLE event_log ALTER COLUMN event_number DROP NOT NULL;
-ALTER TABLE event_log ALTER COLUMN event_number DROP DEFAULT;
+ALTER TABLE event_log ALTER COLUMN event_number SET DEFAULT VALUE nextval('event_sequence_seq'::regclass);;
+ALTER TABLE event_log ALTER COLUMN event_number SET NOT NULL;

e32df5c2ad999fafdfae66bf1cc1542b8e9fa34f
zeldal-hmcts <zeldal.ozdemir1@hmcts.net>
2025-12-19 15:37:23 +0000

    PEG-2669 Create Proactive event linking with in memory events

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,10 +4,15 @@ on [Keep a CHANGELOG](http://keepachangelog.com/). This project adheres to
 [Semantic Versioning](http://semver.org/).
 
 ### [Unreleased]
+## Added
+- Notification-based event linking and publishing via CDI events, enabled via JNDI:
+  - event.linking.worker.notified (linking)
+  - event.publishing.worker.notified (publishing)
+- Bug fixes: dead notifier thread recovery, submit() failure handling
 
 ### [17.104.1-M1]  - 2026-03-17
 ### Changed
-- Batch event linking: `EventNumberLinker` now links N events per JTA transaction using JDBC `executeBatch()`, 
+- Batch event linking: `EventNumberLinker` now links N events per JTA transaction using JDBC `executeBatch()`,
   configurable via JNDI `event.linking.worker.batch.size` (default 10)
 
 # [17.104.0] - 2025-12-16
---
+++ event-sourcing/event-publisher/event-publisher-timer/src/main/java/uk/gov/justice/services/eventsourcing/eventpublishing/EventLinkingNotifier.java
@@ -0,0 +1,88 @@
+package uk.gov.justice.services.eventsourcing.eventpublishing;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import uk.gov.justice.services.eventsourcing.eventpublishing.configuration.EventLinkingWorkerConfig;
+import uk.gov.justice.services.eventsourcing.repository.jdbc.event.EventAppendedEvent;
+
+import javax.annotation.Resource;
+import javax.ejb.Singleton;
+import javax.enterprise.concurrent.ManagedExecutorService;
+import javax.enterprise.event.Observes;
+import javax.inject.Inject;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+@Singleton
+public class EventLinkingNotifier {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(EventLinkingNotifier.class);
+
+    @Inject
+    private EventNumberLinker eventNumberLinker;
+
+    @Inject
+    private EventLinkingWorkerConfig eventLinkingWorkerConfig;
+
+    @Inject
+    private EventPublishingNotifier eventPublishingNotifier;
+
+    @Resource
+    private ManagedExecutorService managedExecutorService;
+
+    private final AtomicBoolean started = new AtomicBoolean(false);
+    private final Object monitor = new Object();
+
+
+    public void onEventAppendedEvent(@Observes final EventAppendedEvent eventAppendedEvent) {
+        this.wakeUp(false);
+    }
+
+    public void wakeUp(boolean startIfStopped) {
+        if (startIfStopped && started.compareAndSet(false, true)) {
+            managedExecutorService.submit(this::runWithInterruptable);
+        }
+        synchronized (monitor) {
+            monitor.notifyAll();
+        }
+    }
+
+    private void runWithInterruptable() {
+        long currentBackoff = eventLinkingWorkerConfig.getBackoffMinMilliseconds();
+
+        while (!Thread.currentThread().isInterrupted()) {
+            try {
+                if (eventNumberLinker.findAndAndLinkNextUnlinkedEvent()) {
+                    currentBackoff = eventLinkingWorkerConfig.getBackoffMinMilliseconds();
+                    eventPublishingNotifier.wakeUp(false);
+                } else {
+                    synchronized (monitor) {
+                        monitor.wait(currentBackoff);
+                    }
+                    currentBackoff = Math.min(
+                            (long) (currentBackoff * eventLinkingWorkerConfig.getBackoffMultiplier()),
+                            eventLinkingWorkerConfig.getBackoffMaxMilliseconds()
+                    );
+                }
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                LOGGER.info("EventLinkingWorker interrupted, stopping.");
+                break;
+            } catch (Exception e) {
+                LOGGER.error("Error in EventLinkingWorker loop", e);
+                // Ensure we don't spin in a tight loop on error
+                try {
+                    synchronized (monitor) {
+                        monitor.wait(currentBackoff);
+                    }
+                } catch (InterruptedException ie) {
+                    Thread.currentThread().interrupt();
+                    break;
+                }
+                currentBackoff = Math.min(
+                        (long) (currentBackoff * eventLinkingWorkerConfig.getBackoffMultiplier()),
+                        eventLinkingWorkerConfig.getBackoffMaxMilliseconds()
+                );
+            }
+        }
+    }
+}

b62839b958dd4d50a3ce8470db7b2eba06150b58
allanmckenzie <allan.mckenzie@hmcts.net>
2026-01-16 16:19:27 +0000

    Bootstrap event_subscription_status on wildfly startup

---
+++ event-tracking/event-tracking-discovery/src/main/java/uk/gov/justice/eventsourcing/discovery/bootstrap/EventDiscoveryBootstrapper.java
@@ -0,0 +1,4 @@
+package uk.gov.justice.eventsourcing.discovery.bootstrap;
+
+public class Fred {
+}
---
+++ event-tracking/event-tracking-discovery/src/test/java/uk/gov/justice/eventsourcing/discovery/bootstrap/EventDiscoveryBootstrapWildflyExtensionTest.java
@@ -0,0 +1,19 @@
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.sameInstance;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.when;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+public class EventDiscoveryBootstrapWildflyExtensionTest {
+
+  @InjectMocks
+  private uk.gov.justice.eventsourcing.discovery.bootstrap.EventDiscoveryBootstrapWildflyExtension 
+  
+  
+}

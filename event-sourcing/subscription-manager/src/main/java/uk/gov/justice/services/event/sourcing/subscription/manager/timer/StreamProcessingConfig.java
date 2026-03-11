370da55253080b38a6f8eafffd1d876ff2353c4d
santhosh-hmcts <santhosh.kotha@hmcts.net>
2026-03-11 16:24:02 +0000

    PEG-3057 Use lazy value to return jndi config value

--- event-sourcing/subscription-manager/src/main/java/uk/gov/justice/services/event/sourcing/subscription/manager/timer/StreamProcessingConfig.java
+++ event-sourcing/subscription-manager/src/main/java/uk/gov/justice/services/event/sourcing/subscription/manager/timer/StreamProcessingConfig.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import uk.gov.justice.services.common.configuration.Value;
+import uk.gov.justice.services.common.util.LazyValue;
 
 import static java.lang.Boolean.parseBoolean;
 import static java.lang.Integer.parseInt;
@@ -11,6 +12,17 @@ import static java.lang.Long.parseLong;
 
 @Singleton
 public class StreamProcessingConfig {
+
+    private final LazyValue timerStartWaitLazyValue = new LazyValue();
+    private final LazyValue timerIntervalLazyValue = new LazyValue();
+    private final LazyValue maxWorkersLazyValue = new LazyValue();
+    private final LazyValue idleThresholdLazyValue = new LazyValue();
+    private final LazyValue maxRetriesLazyValue = new LazyValue();
+    private final LazyValue retryDelayLazyValue = new LazyValue();
+    private final LazyValue retryDelayMultiplierLazyValue = new LazyValue();
+    private final LazyValue accessEventStoreViaRestLazyValue = new LazyValue();
+    private final LazyValue circuitBreakerFailureThresholdLazyValue = new LazyValue();
+    private final LazyValue circuitBreakerCoolDownLazyValue = new LazyValue();
 
     @Inject
     @Value(key = "stream.processing.timer.start.wait.milliseconds", defaultValue = "7250")
@@ -53,42 +65,42 @@ public class StreamProcessingConfig {
     private String circuitBreakerCoolDownMilliseconds;
 
     public long getTimerStartWaitMilliseconds() {
-        return parseLong(timerStartWaitMilliseconds);
+        return timerStartWaitLazyValue.createIfAbsent(() -> parseLong(timerStartWaitMilliseconds));
     }
 
     public long getTimerIntervalMilliseconds() {
-        return parseLong(timerIntervalMilliseconds);
+        return timerIntervalLazyValue.createIfAbsent(() -> parseLong(timerIntervalMilliseconds));
     }
 
     public int getMaxWorkers() {
-        return parseInt(maxWorkers);
+        return maxWorkersLazyValue.createIfAbsent(() -> parseInt(maxWorkers));
     }
 
     public long getIdleThresholdMilliseconds() {
-        return parseLong(idleThresholdMilliseconds);
+        return idleThresholdLazyValue.createIfAbsent(() -> parseLong(idleThresholdMilliseconds));
     }
 
     public Integer getMaxRetries() {
-        return parseInt(maxRetries);
+        return maxRetriesLazyValue.createIfAbsent(() -> parseInt(maxRetries));
     }
 
     public Long getRetryDelayMilliseconds() {
-        return parseLong(retryDelayMilliseconds);
+        return retryDelayLazyValue.createIfAbsent(() -> parseLong(retryDelayMilliseconds));
     }
 
     public BigDecimal getRetryDelayMultiplier() {
-        return new BigDecimal(retryDelayMultiplier);
+        return retryDelayMultiplierLazyValue.createIfAbsent(() -> new BigDecimal(retryDelayMultiplier));
     }
 
     public boolean accessEventStoreViaRest() {
-        return parseBoolean(accessEventStoreViaRest);
+        return accessEventStoreViaRestLazyValue.createIfAbsent(() -> parseBoolean(accessEventStoreViaRest));
     }
 
     public int getCircuitBreakerFailureThreshold() {
-        return parseInt(circuitBreakerFailureThreshold);
+        return circuitBreakerFailureThresholdLazyValue.createIfAbsent(() -> parseInt(circuitBreakerFailureThreshold));
     }
 
     public long getCircuitBreakerCoolDownMilliseconds() {
-        return parseLong(circuitBreakerCoolDownMilliseconds);
+        return circuitBreakerCoolDownLazyValue.createIfAbsent(() -> parseLong(circuitBreakerCoolDownMilliseconds));
     }
 }
